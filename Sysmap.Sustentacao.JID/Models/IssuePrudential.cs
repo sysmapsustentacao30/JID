3e5c34eaeb3a1772c24537dbee17050061bbd4a0
Marcelo Martins Oliveira <marcelo.martins@sysmap.com.br>
2019-05-06 16:43:50 -0300

    Add Project JID

---
+++ Sysmap.Sustentacao.JID/Models/IssuePrudential.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sysmap.Sustentacao.JID.Models
+{
+    public class IssuePrudential
+    {
+        public int ID { get; set; }
+
+        public string Summary { get; set; }
+
+        public string IssueType { get; set; }
+
+        public string ServiceNow { get; set; }
+
+        public int WexId { get; set; }
+
+        public string Status { get; set; }
+    }
+}
---
+++ Sysmap.Sustentacao.JID/Models/WexPlan.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sysmap.Sustentacao.JID.Models
+{
+    public class WexPlan
+    {
+        public int IdWex { get; set; }
+
+        public string OrdemServico { get; set; }
+
+        public string Responsavel { get; set; }
+
+        public string Documento { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime Data { get; set; }
+    }
+}
